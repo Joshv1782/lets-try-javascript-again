a6374b30eb955d004ebaa8d21eb8b0f124da26c0
Josh <jvandenbrink1782@gmail.com>
2020-03-09 11:17:03 -0600

    added css and changed the hambuger menu

--- js/javascript.js
+++ js/javascript.js
@@ -145,38 +145,39 @@ myHeading.className = "grey-text"; // Use classes instead!
   // \r\n is return/newline - this will now output on TWO lines in your console.
 
   /**
-   * Let's review some condidtions-rekated operators
-   */
+ * Let's review some conditions-related operators...
+ */
 
-   // check for equivalency (type-agnostic)
-   console.log( 1 == true ); // True
-   console.log( "Hello, World!" == true ); // True
-   console.log( -137 == true ); // Flase
-   console.log( "test" == "test" ); // True
-   console.log( 36 =="36" ); // True
-   console.log( 0 == false ); // False
-   console.log( "test string" == "test string")
+// Check for equivalency (type-agnostic.)
+console.log( 1 == true ); // True
+console.log( 'Hello, World!' == true ); // False
+console.log( -137 == true ); // False
+console.log( 'test' == 'test' ); // True
+console.log( 36 == '36' ); // True
+console.log( 0 == false ); // True
+console.log( `test string` == "test string" ); // True
 
-   // check if identical (value comparison including type)
-   console.log( 1=== true ); // False
-   console.log( "" === true ); // Flase
-   console.log( -137 == true ); // False
-   console.log( "test" === "test" ); // True
-   console.log( 36 ==="36" ); // Flase
-   console.log( 0 === false ); // False
+// Check if identical (value comparison including type.)
+console.log( 1 === true ); // False
+console.log( '' === true ); // False
+console.log( -137 === true ); // False
+console.log( 'test' === 'test' ); // True
+console.log( 36 === '36' ); // False
+console.log( 0 === false ); // False
+console.log( `test string` === "test string" ); // True
 
-   /**
-    * Hamburger menu
-    */
-   
-    // Let's grab out menu...
-  var MyNav = document.querySelector( "nav" );
+/**
+ * Hamburger Menu.
+ */
 
-  // And our menu button...
-  var myNavButton = document.querySelector( ".menu-button" );
+// Let's grab our menu...
+var myNav = document.querySelector( 'nav' );
 
-  // Let's listen for a click on this
-  myNavButton.addEvenetListener( "click", function (event) {
-      // When clicked, add/remove the "nav-open" class (in HTML)
-    myNavButton.classList.toggle( "nav-open" );
-  } );
+// And our menu button...
+var myNavButton = document.querySelector( '.menu-button' );
+
+// Let's listen for a click on this.
+myNavButton.addEventListener( 'click', function ( event ) {
+    // When clicked, add/remove the "nav-open" class (in HTML.)
+    myNav.classList.toggle( 'nav-open' );
+} );
